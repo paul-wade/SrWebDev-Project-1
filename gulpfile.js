4357540028fd99f23d2e333a80ceb18d367e4bdb
Paul Wade <paul.wade@live.com>
2016-09-05 23:07:10 -0500

    adding mocha framework

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,34 @@
 var gulp = require('gulp');
 var gulpPug = require('gulp-pug');
 var gulpStylus = require('gulp-stylus');
+var concat = require('gulp-concat');
+var eslint = require('gulp-eslint');
+var mocha = require('gulp-mocha');
 
 gulp.task('views', function buildHTML() {
     return gulp.src('views/*.pug')
         .pipe(gulpPug()).pipe(gulp.dest('dist'));
-
 });
 
-gulp.task('styleSheets',function () {
+gulp.task('styleSheets', function () {
     return gulp.src('styles/*.styl')
-        .pipe(gulpStylus()).pipe(gulp.dest('dist'));
+        .pipe(gulpStylus())
+        .pipe(concat('styles.css'))
+        .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['views','styleSheets']);
+gulp.task('lint', function () {
+    return gulp.src(['**/*.js', '!node_modules/**'])
+        .pipe(eslint())
+        .pipe(eslint.format())
+        .pipe(eslint.failAfterError());
+});
+
+gulp.task('test', function () {
+    return gulp.src('./tests/*.spec.js').pipe(mocha({reporter: 'nyan'}));
+});
+
+gulp.task('scripts', function () {
+});
+
+gulp.task('default', ['views', 'styleSheets', 'lint','test', 'scripts']);
---
+++ tests/account.spec.js
@@ -0,0 +1,11 @@
+var chai = require('chai');
+var describe = require("mocha").describe;
+var it = require("mocha").it;
+var should = chai.should();
+
+describe('test', function () {
+    it('should do somthing', function () {
+        var test ="test";
+        test.should.equal("tewst");
+    });
+});
