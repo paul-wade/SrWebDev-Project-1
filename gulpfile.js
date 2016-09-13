fe00c8f1a4e443eb3508d32abec8b0e43da5ee70
Paul Wade <paul.wade@live.com>
2016-09-12 23:05:13 -0500

    wrapping up register form, fixed navigation

--- gulpfile.js
+++ gulpfile.js
@@ -9,34 +9,34 @@ var uglify = require('gulp-uglify');
 
 gulp.task('views', function () {
     return gulp.src(['views/*.pug', '!views/layout.pug'])
-        .pipe(gulpPug()).pipe(gulp.dest('dist'));
+               .pipe(gulpPug()).pipe(gulp.dest('dist'));
 });
 
 gulp.task('styleSheets', function () {
     return gulp.src('styles/*.styl')
-        .pipe(gulpStylus())
-        .pipe(concat('styles.css'))
-        .pipe(gulp.dest('dist'));
+               .pipe(gulpStylus())
+               .pipe(concat('styles.css'))
+               .pipe(gulp.dest('dist'));
 });
 
 gulp.task('vendor', function () {
     return gulp.src(['node_modules/bootstrap/dist/css/bootstrap.min.css',
-        'node_modules/bootstrap/dist/css/bootstrap-theme.css',
-        'node_modules/bootstrap/dist/js/bootstrap.js',
-        'node_modules/font-awesome/css/font-awesome.min.css'])
-        .pipe(gulp.dest('dist'));
+                     'node_modules/bootstrap/dist/css/bootstrap-theme.css',
+                     'node_modules/bootstrap/dist/js/bootstrap.js',
+                     'node_modules/font-awesome/css/font-awesome.min.css'])
+               .pipe(gulp.dest('dist'));
 });
 
 gulp.task('fonts', function () {
     return gulp.src('node_modules/font-awesome/fonts/**/*')
-        .pipe(gulp.dest('dist/fonts'));
+               .pipe(gulp.dest('dist/fonts'));
 });
 
 gulp.task('lint', function () {
     return gulp.src(['js/*.js', '!node_modules/**'])
-        .pipe(eslint())
-        .pipe(eslint.format())
-        .pipe(eslint.failAfterError());
+               .pipe(eslint())
+               .pipe(eslint.format())
+               .pipe(eslint.failAfterError());
 });
 
 gulp.task('test', function () {
@@ -45,10 +45,16 @@ gulp.task('test', function () {
 
 gulp.task('scripts', function () {
     return gulp.src('js/*.js')
-        .pipe(concat('app.js'))
-        .pipe(rename('app.min.js'))
-        //.pipe(uglify())
-        .pipe(gulp.dest('dist'));
+               .pipe(concat('app.js'))
+               .pipe(rename('app.min.js'))
+               //.pipe(uglify())
+               .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['views', 'styleSheets', 'vendor','fonts', 'lint', 'test', 'scripts']);
+var alltasks = ['views', 'styleSheets', 'vendor', 'fonts', 'lint', 'test', 'scripts'];
+
+gulp.task('default', alltasks);
+
+gulp.task('watch', function () {
+    gulp.watch(['js/*.js', 'styles/*.styl','views/*.pug'], alltasks);
+});
